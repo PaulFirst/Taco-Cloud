71f355c7fce066effce88a417e063d88a4387cdf
Pavel Maslov <pavelm320@gmail.com>
2022-01-24 21:53:52 +0700

    project created

---
+++ src/test/java/com/pavelm/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavelm.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

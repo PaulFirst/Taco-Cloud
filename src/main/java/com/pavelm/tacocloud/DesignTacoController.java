fe5188ef0bcdd133fb87e2f59909b80d52d85943
Pavel Maslov <pavelm320@gmail.com>
2022-01-26 19:45:12 +0700

    validation added to post processing

--- src/main/java/com/pavelm/tacocloud/DesignTacoController.java
+++ src/main/java/com/pavelm/tacocloud/DesignTacoController.java
@@ -3,6 +3,7 @@ package com.pavelm.tacocloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.pavelm.tacocloud.Ingredient.Type;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -44,7 +47,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Design design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
